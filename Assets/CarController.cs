c13d16dcfabd528365a9a54d10efb6b2c64b4999
Unknown <qiany@email.arizona.edu>
2018-10-09 12:52:41 -0700

    map

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
+    float speedForce =-20f;
+    float torqueForce = -2f;
 	// Use this for initialization
 	void Start () {
 		
 	}
+    void Update(){
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+		if(Input.GetButtonDown("Accelerate")){
+            rb.AddForce( transform.right * speedForce);
+        }
+        rb.AddTorque(Input.GetAxis("Horizontal") * torqueForce);
+
+        //rb.velocity = ForwardVelocity();
 	}
+/*
+    Vector2 ForwardVelocity(){
+        return transform.up * Vector2.Dot( GetComponent<Rigidbody2D>().velocity, transform.right);
+    }
+    Vector2 RightVelocity(){
+        return transform.up * Vector2.Dot( GetComponent<Rigidbody2D>().velocity, transform.right);
+    }*/
 }
